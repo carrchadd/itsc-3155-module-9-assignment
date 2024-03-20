907e2713bb55946dab4f280020ead5a16fb71336
Sarah <saraharguetaluv@gmail.com>
2024-03-20 18:25:19 -0400

    create_feature completed

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,7 @@
 # TODO: Feature 2
+from flask.testing import FlaskClient
+
+def test_create_movie(test_app:FlaskClient):
+    response = test_app.get('/movies')
+    assert response.status_code == 200
+    
