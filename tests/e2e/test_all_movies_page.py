f21fa76e975956319f5ca3a04e30a1da9509cf67
Chad <107705900+carrchadd@users.noreply.github.com>
2024-03-20 22:48:17 -0400

    Adding tests

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,27 @@
-# TODO: Feature 1
+from src.repositories.movie_repository import get_movie_repository
+
+
+movie_repository = get_movie_repository()
+movie_repository.create_movie('Avengers', 'Anthony Russo', 5)
+movie_repository.create_movie('Cars 2', 'John Lasseter', 7)
+
+def test_get_movies_empty(test_app):
+	response = test_app.get('/movies')
+
+	data = response.data.decode('utf-8')
+
+	assert response.status_code == 200
+	assert '<h1 class="mt-5 text-center">No Movies To Display</h1>' not in data
+
+def test_get_all_movies(test_app):
+	response = test_app.get('/movies')
+
+	data = response.data.decode('utf-8')
+
+	assert response.status_code == 200
+	assert '<td>Avengers</td>' in data
+	assert '<td>Anthony Russo</td>' in data
+	assert '<td>5</td>' in data
+	assert '<td>Cars 2</td>' in data
+	assert '<td>John Lasseter</td>' in data
+	assert '<td>7</td>' in data
