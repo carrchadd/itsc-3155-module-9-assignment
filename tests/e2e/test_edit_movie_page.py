93ae5022f57bcda14737945f515b5bb038855834
Carson Bavery <ctrackb31@gmail.com>
2024-03-20 20:48:12 -0400

    Feature 5 implemented, Tests completeted

--- tests/e2e/test_edit_movie_page.py
+++ tests/e2e/test_edit_movie_page.py
@@ -1,1 +1,9 @@
-# TODO: Feature 5
+from app import app
+
+def test_edit_movie_page():
+    test_app = app.test_client()
+
+    response = test_app.get('/movies/1/edit')
+
+    assert response.status_code == 200
+    assert b'<label for="title" class="form-label">Title</label>' in response.data
